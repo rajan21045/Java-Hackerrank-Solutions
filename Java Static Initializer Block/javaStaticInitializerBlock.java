9855ed7aed3c973767f1e0c06fc55c43ec437421
Rajan Poudel <raajan.works@gmail.com>
2025-08-18 23:31:26 +0545

    Update javaStaticInitializerBlock.java

--- Java Static Initializer Block/javaStaticInitializerBlock.java
+++ Java Static Initializer Block/javaStaticInitializerBlock.java
@@ -4,7 +4,7 @@ Static initialization blocks are executed when the class is loaded, and you can 
 It's time to test your knowledge of Static initialization blocks. You can read about it here.
 
 You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth B and height H. You should read the variables from the standard input.
-
+ 
 If B<=0 or H<=0 , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
 
 Input Format
